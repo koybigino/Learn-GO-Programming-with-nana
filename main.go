6b24965258ca2cb5f28fe09624d7c774d4e5557f
koybigino <koybigino@gmail.com>
2022-08-08 11:05:43 +0100

    Data type 1

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = conferenceTickets
+	var remainingTickets int = conferenceTickets
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
